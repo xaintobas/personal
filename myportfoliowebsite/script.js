1ab5ef0dd870ae80d0784099a38d45b9374de28d
xaintobas <etsxaint@gmail.com>
2024-08-11 04:44:21 +0100

    Added the date time function

--- myportfoliowebsite/script.js
+++ myportfoliowebsite/script.js
@@ -4,6 +4,8 @@ const btnCloseEl = document.querySelector('.btn-close');
 const asideContentEl = document.querySelector('.aside-content');
 const mainContentEl =  document.querySelector('.main-content');
 const overlayEl = document.querySelector('.overlay');
+const timeNowEl = document.querySelector('.time-now');
+const dateNowEl = document.querySelector('.date-now');
 
 
 // FUNCTION TO OPEN AND CLOSE SIDE CONTENT
@@ -19,9 +21,27 @@ const closeSideContent = function () {
   overlayEl.classList.add('hidden');
 };
 
-
 btnOpenEl.addEventListener('click', openSideContent);
 overlayEl.addEventListener('click', closeSideContent);
 btnCloseEl.addEventListener('click', closeSideContent);
 
 
+// GET THE CURRENT TIME AND DAY
+
+const date = new Date();
+const day = date.getDay();
+const currentHours = date.getHours();
+const currentMins = date.getMinutes();
+
+const getCurrentTime = currentHours >= 12 ? `${currentHours} : ${currentMins} PM` : `${currentHours} : ${currentMins} AM`;
+
+timeNowEl.textContent = getCurrentTime;
+
+const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+for (let i = 0; i <= days.length; i++) {
+  if (day === i) {
+    // console.log(days[i]);
+    dateNowEl.textContent = `Today is: ${days[i]}`;
+  }
+}
